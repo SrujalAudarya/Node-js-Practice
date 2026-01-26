7d3902b20b4e825682c77edc619fc3a26c1509a8
SrujalAudarya <srujalaudarya249@gmail.com>
2026-01-26 13:16:40 +0530

    add new practical folder and start learning node js

---
+++ pratical/calculate.js
@@ -0,0 +1,24 @@
+let sum = (a, b) => {
+    return a + b;
+}
+
+let diff = (a, b) => {
+    return a - b;
+}
+
+let prod = (a, b) => {
+    return a * b;
+}
+
+let div = (a, b) => {
+    return a / b;
+}
+
+module.exports = { 
+    sum, 
+    diff, 
+    prod, 
+    div 
+};
+
+create a wishlist screen where when i click on heart icon of any screen product then that product is stored at wishlist screen. Now in my order page when i click on product then in track order section display the order track properly and when i click on download invoice option then download one sample invoice properly just like flipkart app.Now in my profile page add one change user profile image option where user can add there profile image. Now when i click on any product then open a product screen in that product screen add one ask to ai option where any one can talk to the ai that recommend the proper product and at home page add one floating button when click on that then open one pop up screen where user can ask there query and also give there requirement on that basis ai can recommend the product with comparsion for that all ai work i can provide you gemini api key = AIzaSyBum-gnAlshm0aRrSvDc00KUsCEGxCWghA. now in product screen when i click buy now option then order page is open but in that at price session not display the price it can display the $nan fix it and add more 30+ multiple product and at home screen dislay the product on category session with proper in that category and you can add multiple category which are displayed on home screen and also when i click on that then also display the that category products.
